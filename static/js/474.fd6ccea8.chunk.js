0f34983bf780884dc313b2a6c4dd680a8a28e59e
Vasiliy8P <109871157+Vasiliy8P@users.noreply.github.com>
2023-04-26 20:55:26 +0000

    Deploying to gh-pages from @ Vasiliy8P/tweets-cards@50ef62273a711af9db2df38c28d4e1633e95b5df 🚀

---
+++ static/js/474.fd6ccea8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[474],{474:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var s=n(1087),r=n(184),a=function(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)("h1",{children:"User page"}),(0,r.jsx)(s.OL,{to:"/tweets",className:"Btn-tweets-link",children:(0,r.jsx)("button",{className:"Btn-tweets",type:"button",to:"/tweets",children:"Tweets"})})]})}}}]);
+//# sourceMappingURL=474.fd6ccea8.chunk.js.map
