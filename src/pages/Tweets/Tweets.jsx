50ef62273a711af9db2df38c28d4e1633e95b5df
Vasiliy Pshenychniuk <vasiliy8@gmail.com>
2023-04-26 23:54:23 +0300

    fixed

--- src/pages/Tweets/Tweets.jsx
+++ src/pages/Tweets/Tweets.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { nanoid } from 'nanoid';
-import bootstrap from 'bootstrap'
+import 'bootstrap/js/dist/dropdown';
 import { getDecrementFollowersApi, getIncrementFollowersApi, getTweetsApi } from 'services/api';
 import TweetsCard from 'components/TweetsCard/TweetsCard';
 import Loader from 'components/Loader/Loader';
